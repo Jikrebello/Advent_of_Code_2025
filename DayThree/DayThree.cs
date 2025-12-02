dcfc442d0e22c165a680dda45763f27fdd31303b
James Rebello <James.Rebello@kerridgecs.com>
2025-12-02 15:11:55 +0200

    Refactor input handling and improve project structure
    Refactored `Program.cs` to delegate `DayTwo` logic to a new
    `DayTwo` class, encapsulating file processing and validation.
    Introduced a `DayThree` class to handle future logic for
    `DayThreeInput.txt`. Updated `Advent_of_Code.csproj` to optimize
    file copying behavior by setting `<CopyToOutputDirectory>` to
    `PreserveNewest` for `DayTwoInput.txt` and adding support for
    `DayThreeInput.txt`. Added `DayThreeInput.txt` file.

---
+++ DayThree/DayThree.cs
@@ -0,0 +1,15 @@
+﻿namespace Advent_of_Code.DayThree
+{
+	internal class DayThree
+	{
+		public DayThree()
+		{
+			// load the file from disk
+			string filePath = $@"{AppContext.BaseDirectory}\DayThree\DayThreeInput.txt";
+
+			string content = File.ReadAllText(filePath);
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ DayTwo/DayTwo.cs
@@ -0,0 +1,73 @@
+﻿namespace Advent_of_Code.DayTwo
+{
+	internal class DayTwo
+	{
+		public DayTwo()
+		{
+			// load the file from disk
+			string filePath = $@"{AppContext.BaseDirectory}\DayTwo\DayTwoInput.txt";
+
+			string content = File.ReadAllText(filePath);
+			string[] productIds = content.Split(',');
+
+			long invalidProductIdCount = 0;
+
+			string[][] ranges = [.. productIds
+				.Select(id => id.Split('-'))
+				.Select(parts => new string[] { parts[0], parts[1] })];
+
+			//loop to go through the whole 2D array
+			for (int i = 0; i < ranges.Length; i++)
+			{
+				long begin = long.Parse(ranges[i][0]);
+				long finish = long.Parse(ranges[i][1]);
+
+				for (long currentRange = begin; currentRange <= finish; currentRange++)
+				{
+					if (IsInvalidId(currentRange))
+					{
+						Console.WriteLine($"Found invalid id {currentRange}");
+						invalidProductIdCount += currentRange;
+					}
+				}
+			}
+
+			Console.WriteLine($"Invalid Product Count total: {invalidProductIdCount}");
+			Console.ReadLine();
+		}
+
+		private static bool IsInvalidId(long id)
+		{
+			int idLength = id.ToString().Length;
+
+			if (idLength == 1)
+				return false;
+
+			for (int patternLength = 1; patternLength <= idLength / 2; patternLength++)
+			{
+				// pattern length must divide evenly into id length
+				if (idLength % patternLength != 0)
+				{
+					continue;
+				}
+
+				string pattern = id.ToString().Substring(0, patternLength);
+
+				// how many times to repeat the pattern
+				int repeatCount = idLength / patternLength;
+				string rebuilt = "";
+				for (int i = 0; i < repeatCount; i++)
+				{
+					rebuilt += pattern;
+				}
+
+				if (rebuilt == id.ToString())
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
