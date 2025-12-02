9de60d533659a7030eb31f51b0af932a1e99e569
James Rebello <James.Rebello@kerridgecs.com>
2025-12-02 14:59:18 +0200

    Refactor product ID validation logic
    Refactored the product ID validation by introducing the `IsInvalidId` helper method to encapsulate the logic for identifying invalid IDs. Simplified the main loop by replacing inline validation logic with a method call and iterating directly over ranges. Consolidated even and odd digit checks into the helper method.
    
    Added new logic to detect invalid IDs based on repeating patterns of digits. Removed redundant code, improved readability, and streamlined the overall structure.

--- Program.cs
+++ Program.cs
@@ -2,10 +2,8 @@
 string filePath = $@"{AppContext.BaseDirectory}\DayTwo\DayTwoInput.txt";
 
 string content = File.ReadAllText(filePath);
-
 string[] productIds = content.Split(',');
 
-long currentRange = 0;
 long invalidProductIdCount = 0;
 
 string[][] ranges = [.. productIds
@@ -15,48 +13,52 @@ string[][] ranges = [.. productIds
 //loop to go through the whole 2D array
 for (int i = 0; i < ranges.Length; i++)
 {
-	// starting at the first value in the first dimension of the array, count sequentially up to the value in the second dimension of the array
-	currentRange = long.Parse(ranges[i][0]);
-	for (long j = long.Parse(ranges[i][0]); j <= long.Parse(ranges[i][1]); j++)
+	long begin = long.Parse(ranges[i][0]);
+	long finish = long.Parse(ranges[i][1]);
+
+	for (long currentRange = begin; currentRange <= finish; currentRange++)
 	{
-		// check one: logic that focuses on if there is an even number of digits
-		if (currentRange.ToString().Length % 2 == 0)
+		if (IsInvalidId(currentRange))
 		{
-			int length = currentRange.ToString().Length;
-			int halfLength = length / 2;
-
-			string firstHalf = currentRange.ToString()[..halfLength];
-			string secondHalf = currentRange.ToString()[halfLength..];
-
-			if (firstHalf == secondHalf)
-			{
-				Console.WriteLine($"Found invalid id {currentRange}");
-				invalidProductIdCount += currentRange;
-			}
-			else if (firstHalf.Length > 2 && secondHalf.Length > 2)
-			{
-				string evenFirstHalf = firstHalf + secondHalf[0];
-				string evenSecondHalf = secondHalf + firstHalf[0];
-
-				string reverseEvenFirstHalf = new(evenFirstHalf.Reverse().ToArray());
-
-				if (reverseEvenFirstHalf == evenSecondHalf)
-				{
-					Console.WriteLine($"Found invalid id {currentRange}");
-					invalidProductIdCount += currentRange;
-				}
-			}
+			Console.WriteLine($"Found invalid id {currentRange}");
+			invalidProductIdCount += currentRange;
 		}
-
-		// check two: logic that focuses on if there is an odd number of digits
-		else
-		{
-			// eg) we have 123123123
-		}
-		currentRange++;
 	}
 }
 
 Console.WriteLine($"Invalid Product Count total: {invalidProductIdCount}");
-
 Console.ReadLine();
+
+static bool IsInvalidId(long id)
+{
+	int idLength = id.ToString().Length;
+
+	if (idLength == 1)
+		return false;
+
+	for (int patternLength = 1; patternLength <= idLength / 2; patternLength++)
+	{
+		// pattern length must divide evenly into id length
+		if (idLength % patternLength != 0)
+		{
+			continue;
+		}
+
+		string pattern = id.ToString().Substring(0, patternLength);
+
+		// how many times to repeat the pattern
+		int repeatCount = idLength / patternLength;
+		string rebuilt = "";
+		for (int i = 0; i < repeatCount; i++)
+		{
+			rebuilt += pattern;
+		}
+
+		if (rebuilt == id.ToString())
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
